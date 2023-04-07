563f96f704fef9c5a4d1ffaad13d619310459f78
Lynne Liu <lynne.liukail@gmail.com>
2023-04-07 17:49:12 -0400

    modified the LED display, so that it display the color that user cannot pick.

--- main.c
+++ main.c
@@ -66,12 +66,13 @@
 #include <time.h>
 
 /* function declare */
-void initial_setup(short int color[5], int idx[25]);
+void initial_setup(int color[5], int idx[25]);
 void wait_for_vsync();
-void drawBoxInitial(int coor_x, int coor_y, short int color);
+void drawBoxInitial(int coor_x, int coor_y, int color);
 void clear_screen_init();
-void draw_pixel(int x, int y, short int line_color);
+void draw_pixel(int x, int y, int line_color);
 void scoreDisplay();
+void colorRestrict(int idx[25], int color[5]);
 
 
 /* global variable */
@@ -88,10 +89,12 @@ int main(void){
     //config_GIC(); // configure the general interrupt controller
 
     // declare other variables
-    short int color[5] = {YELLOW, PINK, CYAN, BLUE, GREY};
+    int color[5] = {YELLOW, PINK, CYAN, BLUE, GREY};
     int idx[25];  //CONTAINER FOR 25 RANDOM
-    // code generates the numbers such that no neighbouring colors are the same as the current colour
-    int grid_width = 5; int grid_height = 5;
+
+    /* code generates the numbers such that no neighbouring colors are the same as the current colour */
+    int grid_width = 5; 
+    int grid_height = 5;
     for (int h = 0; h < grid_height; h++) {
         for (int w = 0; w < grid_width; w++) {
             int color_idx;
@@ -99,12 +102,12 @@ int main(void){
             do {
                 // gets the random color number until it doesn't match its neighbouring colors
                 color_idx = rand() % 5;
-            
             // loop checks if the current color is the color above it or the color beside it, is the same color
-            } while ((h > 0 && idx[(h-1)*grid_width+w] == color_idx) || (w > 0 && idx[h*grid_width+(w-1)] == color_idx));
-            idx[h*grid_width+w] = color_idx;
+            } while ((h > 0 && idx[(h - 1) * grid_width + w] == color_idx) || (w > 0 && idx[h * grid_width + (w - 1)] == color_idx));
+            idx[h * grid_width + w] = color_idx;
         }
     }
+
     /* set front pixel buffer to start of FPGA On-chip memory */
     *(pixel_ctrl_ptr + 1) = FPGA_ONCHIP_BASE; // first store the address in the back buffer
     /* now, swap the front/back buffers, to set the front buffer location */
@@ -119,10 +122,11 @@ int main(void){
     clear_screen_init(); // pixel_buffer_start points to the pixel buffer
     initial_setup(color, idx);
     initial = true;
+    colorRestrict(idx, color);
 
     while (1){
         scoreDisplay();
-        colorRestrict(idx);
+        colorRestrict(idx, color);
         /* change the color for boxes, use interrupt to get user input */
         //SW 0-4: used to switch colors; | 0, Yellow | 1, Pink | 2, Cyan | 3, Blue | 4, Grey |
         //LED 0-5: an indicator for users to tell them which color they cannot pick
@@ -158,7 +162,7 @@ void wait_for_vsync(){
 }
 
 /* draw the colored pixel for initial set up */
-void drawBoxInitial(int coor_x, int coor_y, short int color){  //48 pixels in total
+void drawBoxInitial(int coor_x, int coor_y, int color){  //48 pixels in total
     for(int i = 0; i < 48; i++){
         for(int j = 0; j < 48; j++){
             draw_pixel(coor_x + i, coor_y + j, color);
@@ -176,12 +180,12 @@ void clear_screen_init(){
 }
 
 /* plot a pixel on the VGA display */
-void draw_pixel(int x, int y, short int line_color){
+void draw_pixel(int x, int y, int line_color){
     *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
 }
 
 /* initialization of the original state; changed when restart */
-void initial_setup(short int color[5], int idx[25]){
+void initial_setup(int color[5], int idx[25]){
     int delta_j = 47;
     int delta_i = 47;
     int scale = 0;
@@ -196,22 +200,144 @@ void initial_setup(short int color[5], int idx[25]){
 }  //240 for xy; 5*5; 48 pixels per box;
 
 /* LED indicators */
-void colorRestrict(int idx[25]){
-    if(idx[20] ==YELLOW || idx[4] == YELLOW){
-        int* LED = LEDR_BASE;
-        *LED = 0x7F;
-    }else if(idx[20] == PINK || idx[4] == PINK){
-        int* LED = LEDR_BASE;
-        *LED = 0x3F;
-    }else if(idx[20] == CYAN || idx[4] == CYAN){
-        int* LED = LEDR_BASE;
-        *LED = 0x1F;
-    }else if(idx[20] == BLUE || idx[4] == BLUE){
-        int* LED = LEDR_BASE;
-        *LED = 0xF;
-    }else if(idx[20] == GREY || idx[4] == GREY){
+void colorRestrict(int idx[25], int color[5]){
+    /* boolean values for different color pairs */
+    bool yellow = false;
+    bool pink = false;
+    bool cyan = false;
+    bool blue = false;
+    bool grey = false;
+    /* light the LED for lower left corner and upper right color */
+    //yellow, 1
+    if(color[idx[20]] == YELLOW){
         int* LED = LEDR_BASE;
         *LED = 0x1;
+        yellow = true;
+    }
+    if(color[idx[4]] == YELLOW){
+        int* LED = LEDR_BASE;
+        *LED = 0x1;
+        yellow = true;
+    }
+    //pink, 2
+    if(color[idx[20]] == PINK){
+        if(yellow == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x3;
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x2;
+        }
+        pink = true;
+    }
+    if(color[idx[4]] == PINK){
+        if(yellow == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x3;
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x2;
+        }
+        pink = true;
+    }
+    //cyan, 3
+    if(color[idx[20]] == CYAN){
+        if(yellow == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x5;        
+        }else if(pink == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x6;        
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x4;            
+        }
+        cyan = true;
+    }
+    if(color[idx[4]] == CYAN){
+        if(yellow == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x5;        
+        }else if(pink == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x6;        
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x4;            
+        }
+        cyan = true;
+    }
+    //blue, 4
+    if(color[idx[20]] == BLUE){
+        if(yellow = true){
+            int* LED = LEDR_BASE;
+            *LED = 0x9;
+        }else if(pink == true){
+            int* LED = LEDR_BASE;
+            *LED = 0xA;
+        }else if(cyan == true){
+            int* LED = LEDR_BASE;
+            *LED = 0xC;
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x8;
+        }
+        blue = true;
+    }
+    if(color[idx[4]] == BLUE){
+        if(yellow = true){
+            int* LED = LEDR_BASE;
+            *LED = 0x9;
+        }else if(pink == true){
+            int* LED = LEDR_BASE;
+            *LED = 0xA;
+        }else if(cyan == true){
+            int* LED = LEDR_BASE;
+            *LED = 0xC;
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x8;
+        }
+        blue = true;
+    }
+    //grey, 5
+    if(color[idx[20]] == GREY){
+        if(yellow == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x11;
+        }else if(pink == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x12;
+        }else if(cyan == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x14;
+        }else if(blue == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x18;
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x10;
+        }
+        grey = true;
+    }
+    if(color[idx[4]] == GREY){
+        if(yellow == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x11;
+        }else if(pink == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x12;
+        }else if(cyan == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x14;
+        }else if(blue == true){
+            int* LED = LEDR_BASE;
+            *LED = 0x18;
+        }else{
+            int* LED = LEDR_BASE;
+            *LED = 0x10;
+        }
+        grey = true;
     }
 }
 
